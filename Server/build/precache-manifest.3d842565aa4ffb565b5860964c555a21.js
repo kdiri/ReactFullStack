bd1a4b2fef4728002c05211430100bd7fc7c8b61
Kemal Diri <kemaldiri@gmail.com>
2020-08-27 09:25:25 +0200

    :cyclone: Build the app.

---
+++ Server/build/precache-manifest.3d842565aa4ffb565b5860964c555a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8d6c6651f6706adedc",
+    "url": "/static/css/main.361b16e4.chunk.css"
+  },
+  {
+    "revision": "7c8d6c6651f6706adedc",
+    "url": "/static/js/main.7c8d6c66.chunk.js"
+  },
+  {
+    "revision": "b2a30c43a18ffdfb474a",
+    "url": "/static/js/1.b2a30c43.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f35b99404028ba1516a768d32e7a89d8",
+    "url": "/index.html"
+  }
+];
